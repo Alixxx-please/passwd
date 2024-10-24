8c795f200dc3543b36b8a9afbe844741dfd191c0
aurorarissime <user012120@gmail.com>
2024-10-24 20:15:59 +0200

    install script working properly

--- install.ps1
+++ install.ps1
@@ -2,7 +2,10 @@ $ErrorActionPreference = 'Stop'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $passwdFolderPath = "$env:LOCALAPPDATA\passwd"
+$exePath = "$passwdFolderPath\passwd.exe"
 $process = Get-Process -Name "passwd" -ErrorAction SilentlyContinue
+$latestRelease = Invoke-RestMethod -Uri 'https://api.github.com/repos/Alixxx-please/passwd/releases/latest'
+$exeUrl = $latestRelease.assets[0].browser_download_url
 
 if ($process) {
     Stop-Process -Name "passwd" -Force
@@ -12,8 +15,15 @@ if ($process) {
 }
 
 if (-not (Test-Path -Path $passwdFolderPath)) {
-    New-Item -ItemType Directory -Path $passwdFolderPath
+    New-Item -ItemType Directory -Path $passwdFolderPath | Out-Null
 }
+
+if (Test-Path -Path $exePath) {
+    Remove-Item -Path $exePath -Force
+    Write-Output "Existing passwd.exe has been removed."
+}
+
+Invoke-WebRequest -Uri $exeUrl -OutFile $exePath
 
 $currentPATH = [System.Environment]::GetEnvironmentVariable("PATH", [System.EnvironmentVariableTarget]::User)
 if (-not $currentPATH.Contains($passwdFolderPath)) {
